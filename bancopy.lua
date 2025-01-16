242a51b008bd82123d1351cddb96c2e07f1a448d
SpyderVen0m <126721529+SpyderVen0m@users.noreply.github.com>
2025-01-16 18:33:33 +0000

    jiadja9

---
+++ bancopy.lua
@@ -0,0 +1,26 @@
+local player1 = "sadoj49jv"
+local player2 = "aijdajsda"
+local player3 = "aspijfg"
+local function run(startNumber, endNumber)
+    local running = true
+    while running do 
+        if startNumber == endNumber then 
+            running = false
+        end
+        print(startNumber)
+        startNumber = startNumber + 1
+    end
+end
+
+
+local okTable = {
+    [1] = player1 .. ", ", 
+    [2] = player2 .. ", ", 
+    [3] = player3 .. ", "
+}
+
+table.insert(okTable, 4, "a")
+
+print("\n" .. table.concat(okTable))
+
+run(2, 50)
