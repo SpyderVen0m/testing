4bdb0ef07934e5eb6b7698b1c3db62da0bd371c1
SpyderVen0m <126721529+SpyderVen0m@users.noreply.github.com>
2025-02-13 19:20:12 +0000

    polygon

---
+++ polygon.lua
@@ -0,0 +1,22 @@
+local function run(mode, n)
+    print("\n\n\n")
+    if mode == 1 then
+        local intAngle = (180 * (n - 2)) / n
+        local allAngles = intAngle * n
+        print(n .. ", " .. intAngle .. ", " .. allAngles)
+    end
+
+    if mode == 2 then
+        local sides = 360 / (180 - n)
+        local allAngles = n * sides
+        print(sides .. ", " .. n .. ", " .. allAngles)
+    end
+    
+    if mode == 3 then
+        local sides = (n / 180) + 2
+        local intAngle = n / sides
+        print(sides .. ", " .. intAngle .. ", " .. n)
+    end
+end
+
+run(1, 12)
